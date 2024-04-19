08032df209f407d19d445b98c5b8fcb796d91646
Brandonchen92 <brandon.chen@edu.sait.ca>
2024-04-19 15:26:26 -0600

    Seperate Pages + Navbar

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ fetch(apiUrl)
             .catch(error => {
                 console.error('Error fetching weather data:', error);
             });
+
+
+
+
+// Get the button
+var scrollButton = document.getElementById("scrollBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
